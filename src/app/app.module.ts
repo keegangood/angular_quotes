c39b35805a4e7e9d1299ca4fcae88da318ddf067
unknown <keegood8@gmail.com>
2020-10-11 12:58:29 -0700

    more styling & added spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,9 +7,13 @@ import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
 import { QuoteItemComponent } from './quotes/quote-item/quote-item.component';
 import { SearchComponent } from './quotes/search/search.component';
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { AboutComponent } from './pages/about/about.component';
+import { HomeComponent } from './pages/home/home.component';
+import { SpinnerComponent } from './layout/spinner/spinner.component';
 
 @NgModule({
-  declarations: [AppComponent, QuotesComponent, QuoteItemComponent, SearchComponent],
+  declarations: [AppComponent, QuotesComponent, QuoteItemComponent, SearchComponent, NavbarComponent, AboutComponent, HomeComponent, SpinnerComponent],
   imports: [BrowserModule, HttpClientModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
